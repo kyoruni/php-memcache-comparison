084bd2ef671d358b4c082274d385216156e67e5e
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-07-24 19:39:30 +0900

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# kyoruni/php-memcache-comparison
+
+PHP Memcache クラスの動作差分確認用
+
+## 使い方
+### （1） 立ち上げ
+
+```bash
+docker compose up -d
+```
+
+### （2） 動作確認
+- 以下にアクセス
+- PHP8.3： `http://localhost:8083/`
+- PHP7.3： `http://localhost:8073/`
---
+++ src/index.php
@@ -0,0 +1,31 @@
+<?php
+echo 'PHP version: ' . phpversion() . '<br><br>';
+
+$memcache = new Memcache;
+$memcache->connect('memcached', 11211) or die ("Could not connect");
+
+$keys = [
+  'stringKey'     => 'hogehoge',
+  'stringZeroKey' => '',
+  'intKey'        => 12345,
+  'intZeroKey'    => 0,
+  'boolTrueKey'   => true,
+  'boolFalseKey'  => false,
+  'arrayKey'      => ['red', 'green', 'blue'],
+  'nullKey'       => null
+];
+
+// キャッシュをセット
+foreach ($keys as $key => $value) {
+  $memcache->set($key, $value);
+}
+
+// キャッシュを取得して表示
+foreach ($keys as $key => $value) {
+  $result = $memcache->get($key);
+  $type   = gettype($result);
+
+  echo "$key: ";
+  var_dump($result);
+  echo '<br>';
+}
